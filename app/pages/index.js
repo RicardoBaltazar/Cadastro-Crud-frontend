2e08d2827219c81f10d7f5ab58f3af8cda990830
unknown <ricardo.baltazar04@gmail.com>
2021-03-10 17:24:11 -0300

    componente header/menu

--- app/pages/index.js
+++ app/pages/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 import Container from '../src/Components/Container'
+import Menu from '../src/Components/Menu'
 
 export default function Home() {
   return (
@@ -13,6 +14,12 @@ export default function Home() {
         </title>
       </Head>
       <Container>
+        <Menu 
+        home='#'
+        link1='Home'
+        link2='sobre'
+        link3='bikes'
+        />
         ola mundo
       </Container>
     </div>
